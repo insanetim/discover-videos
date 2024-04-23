fe323bf92c188914d726f5792ec26a4c7688d47a
insanetim <47120724+insanetim@users.noreply.github.com>
2024-04-23 17:01:40 +0300

    added stats

---
+++ pages/api/stats.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken'
+
+import { findVideoByUserId, insertStats, updateStats } from '@/lib/db/hasura'
+
+export default async function stats(req, res) {
+  try {
+    const { token } = req.cookies
+
+    if (!token) {
+      return res.status(403).send({ msg: 'Unauthorized' })
+    }
+
+    const inputParams = req.method === 'POST' ? req.body : req.query
+    const { videoId } = inputParams
+
+    if (!videoId) {
+      return res.status(400).send({ msg: 'Missing videoId' })
+    }
+
+    const { issuer: userId } = jwt.verify(token, process.env.JWT_SECRET)
+    const existingStats = await findVideoByUserId(token, { userId, videoId })
+    const doesStatsExist = existingStats.length > 0
+
+    if (req.method === 'POST') {
+      const { favourited, watched = true } = req.body
+      const statsToUpdate = { userId, videoId, favourited, watched }
+
+      doesStatsExist
+        ? await updateStats(token, statsToUpdate)
+        : await insertStats(token, statsToUpdate)
+
+      return res.send({ done: true })
+    } else if (req.method === 'GET') {
+      return doesStatsExist
+        ? res.send(existingStats)
+        : res.status(404).send({ msg: 'Video not found' })
+    }
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send({ done: false, error: error?.message })
+  }
+}
--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -4,6 +4,8 @@ import Modal from 'react-modal'
 import classNames from 'classnames'
 
 import Navbar from '@/components/Navbar'
+import Like from '@/components/Icons/like-icon'
+import DisLike from '@/components/Icons/dislike-icon'
 
 import { getYoutubeVideoById } from '@/lib/videos'
 
@@ -38,11 +40,28 @@ export async function getStaticPaths() {
 const Video = ({ video }) => {
   const router = useRouter()
   const { videoId } = router.query
-  const [isLoaded, setIsLoaded] = useState(false)
   const modalRef = useRef(null)
   const columnRef = useRef(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [toggleLike, setToggleLike] = useState(false)
+  const [toggleDisLike, setToggleDisLike] = useState(false)
 
   const { title, publishTime, description, channelTitle, viewCount } = video
+
+  useEffect(() => {
+    const fetchVideoData = async () => {
+      const response = await fetch(`/api/stats?videoId=${videoId}`)
+      const data = await response.json()
+
+      if (data.length > 0) {
+        const { favourited } = data[0]
+        setToggleLike(favourited === 1)
+        setToggleDisLike(favourited === 0)
+      }
+    }
+
+    fetchVideoData()
+  }, [videoId])
 
   useEffect(() => {
     const handleResize = () => {
@@ -61,6 +80,32 @@ const Video = ({ video }) => {
       window.removeEventListener('resize', handleResize)
     }
   }, [isLoaded])
+
+  const runRatingService = async favourited => {
+    return await fetch('/api/stats', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ videoId, favourited }),
+    })
+  }
+
+  const handleToggleLike = async () => {
+    const val = !toggleLike
+    setToggleLike(val)
+    setToggleDisLike(!val)
+
+    await runRatingService(val ? 1 : 0)
+  }
+
+  const handleToggleDislike = async () => {
+    const val = !toggleDisLike
+    setToggleDisLike(val)
+    setToggleLike(!val)
+
+    await runRatingService(val ? 0 : 1)
+  }
 
   return (
     <div className={styles.container}>
@@ -82,6 +127,21 @@ const Video = ({ video }) => {
           frameBorder='0'
           onLoad={() => setIsLoaded(true)}
         ></iframe>
+
+        <div className={styles.likeDislikeBtnWrapper}>
+          <div className={styles.likeBtnWrapper}>
+            <button onClick={handleToggleLike}>
+              <div className={styles.btnWrapper}>
+                <Like selected={toggleLike} />
+              </div>
+            </button>
+          </div>
+          <button onClick={handleToggleDislike}>
+            <div className={styles.btnWrapper}>
+              <DisLike selected={toggleDisLike} />
+            </div>
+          </button>
+        </div>
 
         <div className={styles.modalBody}>
           <div className={styles.modalBodyContent}>
